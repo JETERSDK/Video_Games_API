81647c0ca23a5fc22f3fdaf09264662fd0482ed0
Ennin Jeter <jeterennin@gmail.com>
2022-05-12 16:48:07 -0500

    Created Controller file + Updated files to run GET on my Postman

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,30 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            //var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            //return Ok(videoGamePublishers);
+            Console.WriteLine("Endpoint hit");
+            var games = _context.VideoGames;
+            return Ok(games);
+        }
+            
+    }
+}
